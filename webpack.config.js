85044a61a27070960146adca00885841145d9a33
Numan <nomanmani62@gmail.com>
2021-08-14 19:37:29 +0500

    react webpack, sass and babel config completed

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
